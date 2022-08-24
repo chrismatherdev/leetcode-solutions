269cb66ef0c1fb22beba212dbf51936f7a40bff8
Chris Mather <66957634+chrismather410@users.noreply.github.com>
2022-08-24 21:57:55 +0100

    Time: 64 ms (94.16%), Space: 42 MB (94.29%) - LeetHub

---
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.js
@@ -0,0 +1,46 @@
+var runningSum = function(nums) {
+    for(let i = 1; i < nums.length; i++) {
+        nums[i] = nums[i] + nums[i - 1];
+    }
+    
+    return nums;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     for(let i = 1; i < nums.length; i++) {
+//         nums[i] = nums[i] + nums[i - 1];
+//     }
+    
+//     return nums;
