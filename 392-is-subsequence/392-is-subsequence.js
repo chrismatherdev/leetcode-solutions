3160ca581c547727891ee6101f65bfc16898b2d4
Chris Mather <66957634+chrismather410@users.noreply.github.com>
2022-08-29 23:10:31 +0100

    Time: 96 ms (40.90%), Space: 42.1 MB (47.98%) - LeetHub

---
+++ 392-is-subsequence/392-is-subsequence.js
@@ -0,0 +1,9 @@
+var isSubsequence = function(s, t) {
+    let j = 0;
+    
+    for(let i = 0; i < t.length && j < s.length; i++) {
+        if(t[i] == s[j]) j++;
+    }
+    
+    return j === s.length;
+};
