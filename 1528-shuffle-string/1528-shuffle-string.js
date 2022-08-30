ef3b95449d343e46f52728946d3ca460b28cedf6
Chris Mather <66957634+chrismather410@users.noreply.github.com>
2022-08-30 21:33:31 +0100

    Time: 60 ms (99.70%), Space: 44.7 MB (39.64%) - LeetHub

---
+++ 1528-shuffle-string/1528-shuffle-string.js
@@ -0,0 +1,9 @@
+var restoreString = function(s, indices) {
+    let result = [];
+    
+    for(let i = 0; i < s.length; i++) {
+        result[indices[i]] = s[i]
+    }
+    
+    return result.join('');
+};
